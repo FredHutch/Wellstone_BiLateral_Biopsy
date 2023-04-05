229dea888fe9af8e837cc759d717c2eba8cd5615
chaochaowong <cwon2@fredhutch.org>
2023-04-04 17:57:43 -0700

    modified some scripts:

--- scripts/09B-make-comprehensive-df.R
+++ scripts/09B-make-comprehensive-df.R
@@ -31,20 +31,22 @@ css <- readxl::read_xlsx(file.path(pkg_dir, "extdata",
   dplyr::rename(Subject = `Record ID`) %>%
   dplyr::select(Subject, CSS, `Age at this visit`, Sex)
 
-
+#
 # muscle strength
+#
 muscle_strength <- get(load(file.path(pkg_dir, "data", "muscle_strength.rda"))) %>%
   dplyr::rename(Subject=`Record ID`) %>%
   gather(key=`location`, value=`Foot Dorsiflexors`, -Subject) %>%
   dplyr::mutate(sample_id = paste0(Subject, 
                                    str_sub(location, start=1L, end=1L)))
-
+#
 # blood_fat_muscle_content (sum(log10(TPM+1)))
+#
 blood_fat_muscle_content <- blood_fat_muscle_content %>%
   dplyr::select(-RNA_sample_id) %>%
   rename(sample_id = sample_name)
 
-# baskets score
+# baskets score; not going to include
 all_baskets_TPM <- map_dfr(names(all_baskets), function(name) {
   id <- all_baskets[[name]]$gencode_v35    
   assays(dds[id])[["TPM"]] %>% as.data.frame() %>%
@@ -57,6 +59,9 @@ all_baskets_TPM <- map_dfr(names(all_baskets), function(name) {
                 sample_id = str_replace(sample_name, "[b]*_.*", ""), .before="sample_name") %>%
   dplyr::select(-sample_name)  
 
+#
+# baskets score in log10 scale: logSum = sum of log10(basket genes)
+#
 all_baskets_logTPM <- sapply(names(all_baskets), function(name) {
   id <- all_baskets[[name]]$gencode_v35 
   tpm_score <- colSums(log10(assays(dds[id])[["TPM"]]+1))
@@ -65,23 +70,59 @@ all_baskets_logTPM <- sapply(names(all_baskets), function(name) {
   rownames_to_column(var="sample_id") %>%
   dplyr::mutate(sample_id = str_replace(sample_id, "[b]*_.*", "")) 
 
+
 #
-# comprehensive covariates data frame (not include BSS)
-# 
-spread_baskets <- all_baskets_TPM %>%
-  spread(key=basket, value=TPM)
+# complement scores
+#
+
+# 1. tidy up the data frame to be merge to comprehensive_df
+complement_scoring <- readxl::read_xlsx(file.path(pkg_dir, "extdata", 
+                                          "FINAL.Revised.BilatStudy.Path.MACstaining data.FINAL.xlsx"),
+                                sheet=1) %>%
+  dplyr::select(`Record ID`, `Complement Scoring (Left)`, `Complement Scoring (Right)`) %>%
+  tidyr::gather(key=Location, value=`Complement Scoring`, -`Record ID`)   %>%
+  dplyr::mutate(Location = if_else(str_detect(Location, "Left"), "L", "R")) %>%
+  dplyr::mutate(sample_id = paste0(`Record ID`, Location)) %>%
+  dplyr::select(sample_id, `Complement Scoring`)    
+
+
+#
+# comprehensive covariates data frame (not include BSS yet) to include:
+# 1. muscle strength 
+# 2. basket score (logTPM: logSum over basket genes)
+# 3. blood, fat, muscle content
+# 4. complement scoring (using the final scoring)
+#
+
+#spread_baskets <- all_baskets_TPM %>%
+#  spread(key=basket, value=TPM)
 
 comprehensive_df <- mri %>% 
   dplyr::full_join(css, by="Subject") %>%
   dplyr::full_join(muscle_strength %>% dplyr::select(-location, -Subject), by="sample_id") %>%
-  dplyr::full_join(spread_baskets, by="sample_id") %>%
-  dplyr::mutate(`DUX4+ (M6)` = if_else(`DUX4-M6` > 0.3, "DUX4+", "DUX4-")) %>%
-  dplyr::mutate(`DUX4+ (M6)` = factor(`DUX4+ (M6)`, levels=c("DUX4-", "DUX4+"))) %>%
+  #dplyr::full_join(spread_baskets, by="sample_id") %>%
+  #dplyr::mutate(`DUX4+ (M6)` = if_else(`DUX4-M6` > 0.3, "DUX4+", "DUX4-")) %>%
+  #dplyr::mutate(`DUX4+ (M6)` = factor(`DUX4+ (M6)`, levels=c("DUX4-", "DUX4+"))) %>%
   dplyr::full_join(all_baskets_logTPM, by="sample_id") %>%
-  dplyr::full_join(blood_fat_muscle_content, by="sample_id")
+  dplyr::full_join(blood_fat_muscle_content, by="sample_id") %>%
+  dplyr::full_join(complement_scoring, by="sample_id") %>%
+  dplyr::relocate(`Complement Scoring`, .after="Inflammation Score") %>%
+  dplyr::relocate(`CSS`, .after=`Complement Scoring`) %>%
+  dplyr::relocate(`Foot Dorsiflexors`, .after=`CSS`) %>%
+  dplyr::relocate(`Age at this visit`, .after=`sample_id`) %>%
+  dplyr::relocate(`Sex`, .after=`Age at this visit`)
+
+#
+# add class - that classified by ML with longitudinal training data
+#
+load(file.path(pkg_dir, "data", "bilat_MLpredict.rda"))
+comprehensive_df <- comprehensive_df %>%
+  left_join(dplyr::select(bilat_MLpredict, sample_id, class), by="sample_id")
 
 save(comprehensive_df, file = file.path(pkg_dir, "data", "comprehensive_df.rda"))
-writexl::write_xlsx(comprehensive_df, path=file.path(pkg_dir, "stats", "comprehensive_df.xlsx"))
+writexl::write_xlsx(comprehensive_df, path=file.path(pkg_dir, "stats", "comprehensive_metadata.xlsx"))
+
+
 
 #
 # control baskets
